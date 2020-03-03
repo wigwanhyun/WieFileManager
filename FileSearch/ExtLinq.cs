129a298e1869806180807109d10c6e31d7218f71
관현 위 <dnlrhksgus0511@gmail.com>
2020-03-04 00:28:50 +0900

    초기 커밋.

---
+++ FileSearch/ExtLinq.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileSearch
+{
+    static class ExtLinq
+    {
+        public static IEnumerable<T> CatchExceptions<T>(this IEnumerable<T> src, Action<Exception> action = null)
+        {
+            using (var enumerator = src.GetEnumerator())
+            {
+                bool next = true;
+
+                while (next)
+                {
+                    try
+                    {
+                        next = enumerator.MoveNext();
+                    }
+                    catch (Exception ex)
+                    {
+                        if (action != null)
+                        {
+                            action(ex);
+                        }
+                        continue;
+                    }
+
+                    if (next)
+                    {
+                        yield return enumerator.Current;
+                    }
+                }
+            }
+        }
+    }
+}
