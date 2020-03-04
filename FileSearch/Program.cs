e6593703f736db0c6137a43e65881873c88b6399
wigwan <dnlrhksgus0511@rootel.co.kr>
2020-03-04 16:08:51 +0900

    t

--- FileSearch/Program.cs
+++ FileSearch/Program.cs
@@ -1,11 +1,11 @@
 ﻿
-using sun.text.normalizer;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace FileSearch
 {
@@ -13,63 +13,44 @@ namespace FileSearch
     {
         static void Main()
         {
-            string[] needles = CreateNeedles(1500).ToArray();
-            string archiveDirectory = @"D:\";
-            //anyViaContains
 
-            var files =  from retrievedFile  in Directory.EnumerateFiles(archiveDirectory, "*.txt", System.IO.SearchOption.AllDirectories).CatchExceptions() select retrievedFile;
 
             var sw = Stopwatch.StartNew();
+            string archiveDirectory = @"D:\";
+            string searchText = "M00009 삭제할 행을 선택하세요.";
+            var files = from retrievedFile in Directory.EnumerateFiles(archiveDirectory, "*", System.IO.SearchOption.AllDirectories).CatchExceptions() select retrievedFile;
+            var regex = new Regex("((?:.*?)(?:\\.(png|jpg|exe|zip|7z|mp4|dll|pdb|xlsx|pdf|ppt|docx|ico|iso|svn-base|meta|msi|psd)))");
             foreach (var f in files)
             {
-                anyViaAhoCorasick(File.ReadLines(f).ToArray(), "max_line_length = 120");
-            }
-            Console.WriteLine("anyViaContains() took " + sw.Elapsed);
-
-            sw.Restart();
-            foreach (var f in files)
-            {
+                sw.Restart();
+                //zip, png, jpg, exe 등 파일은 제외
                 
-                anyViaAhoCorasick(string.Join(",", File.ReadLines(f)).Split(","), "max_line_length = 120");
+                if (regex.IsMatch(f))
+                {
+                    continue;
+                }
+                else
+                {
+                    try
+                    {
+                        var a = File.ReadLines(f, Encoding.UTF8).CatchExceptions().Any(x => x.Contains(searchText));
+                        if (a == true)
+                        {
+                            Console.WriteLine("success : " + f);
+                        }                        
+                        if(sw.Elapsed > TimeSpan.FromSeconds(1.5))
+                        {
+                            Console.WriteLine("anyContais() took " + sw.Elapsed + " fileNm : " + f);
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        continue;
+                    }
+                }
+                
             }
-            Console.WriteLine("anyViaContains() took " + sw.Elapsed);
-
-            sw.Restart();
-            foreach (var f in files)
-            {
-                var a = File.ReadLines(f).Where(x => x.Contains("max_line_length = 120"));
-                Console.WriteLine(string.Join(",", a));
-            }
-            Console.WriteLine("anyViaAhoCorasick() took " + sw.Elapsed);
-        }
-
-        static bool anyViaContains(string[] needles, string haystack)
-        {
-            return needles.Any(haystack.Contains);
-        }
-
-        static bool anyViaAhoCorasick(string[] needles, string haystack)
-        {
-            var trie = new Trie();
-            trie.Add(needles);
-            trie.Build();
-            return trie.Find(haystack).Any();
-        }
-
-        static IEnumerable<string> CreateNeedles(int n)
-        {
-            for (int i = 0; i < n; ++i)
-                yield return n + "." + n + "." + n;
-        }
-
-        static string createHaystack(int n)
-        {
-            var sb = new StringBuilder();
-
-            for (int i = 0; i < n; ++i)
-                sb.AppendLine("Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text");
-
-            return sb.ToString();
+            
         }
 
     }
